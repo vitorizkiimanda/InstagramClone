541d7c263f4e11d16532db82d2dccf10818f7c1d
Vito Rizki Imanda <vitorizkiimanda@gmail.com>
2019-05-19 16:56:17 +0700

    log in page

--- app/src/main/java/id/codepanda/instagramclone/AuthActivity.kt
+++ app/src/main/java/id/codepanda/instagramclone/AuthActivity.kt
@@ -2,11 +2,24 @@ package id.codepanda.instagramclone
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_auth.*
+import org.jetbrains.anko.intentFor
+import org.jetbrains.anko.noAnimation
 
 class AuthActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
+        onClickListener()
+    }
+
+    private fun onClickListener(){
+        login_button.setOnClickListener { toLogin() }
+    }
+
+    private fun toLogin(){
+        startActivity(intentFor<LoginActivity>())
+        finish()
     }
 }
