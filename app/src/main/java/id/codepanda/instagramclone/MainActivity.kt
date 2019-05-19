494df1d74bed11b3575181985bb2b8ddf825a3ce
Vito Rizki Imanda <vitorizkiimanda@gmail.com>
2019-05-19 14:41:10 +0700

    auth page

--- app/src/main/java/id/codepanda/instagramclone/MainActivity.kt
+++ app/src/main/java/id/codepanda/instagramclone/MainActivity.kt
@@ -3,6 +3,7 @@ package id.codepanda.instagramclone
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
+import id.codepanda.instagramclone.util.showNotif
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,15 +11,12 @@ class MainActivity : AppCompatActivity() {
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
-                message.setText(R.string.title_home)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
                 return@OnNavigationItemSelectedListener true
             }
         }
@@ -30,5 +28,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        notif_button.setOnClickListener { showNotif(this, "Test", "Lorem ipsum dolor sir amet Lorem ipsum dolor sir amet Lorem ipsum dolor sir amet", MainActivity::class.java) }
     }
 }
