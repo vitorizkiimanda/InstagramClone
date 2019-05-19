2e3b3e5174c41e66edc8f31594f1d174f0f7d9d9
Vito Rizki Imanda <vitorizkiimanda@gmail.com>
2019-05-19 21:26:23 +0700

    sign up page

--- app/src/main/java/id/codepanda/instagramclone/RegisterActivity.kt
+++ app/src/main/java/id/codepanda/instagramclone/RegisterActivity.kt
@@ -2,28 +2,22 @@ package id.codepanda.instagramclone
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_auth.*
+import kotlinx.android.synthetic.main.activity_register.*
 import org.jetbrains.anko.intentFor
 
-class AuthActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_auth)
+        setContentView(R.layout.activity_register)
         onClickListener()
     }
-
     private fun onClickListener(){
         login_button.setOnClickListener { toLogin() }
-        signup_email_button.setOnClickListener { toSignup() }
     }
 
     private fun toLogin(){
         startActivity(intentFor<LoginActivity>())
         finish()
-    }
-
-    private fun toSignup(){
-        startActivity(intentFor<RegisterActivity>())
     }
 }
