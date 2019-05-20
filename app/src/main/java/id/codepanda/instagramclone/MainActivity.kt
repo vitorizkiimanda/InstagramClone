a0629d71d7a7bee76a8b95f26afb45b68adb0df0
Vito Rizki Imanda <vitorizkiimanda@gmail.com>
2019-05-20 11:16:23 +0700

    bottom tab icon dynamic

--- app/src/main/java/id/codepanda/instagramclone/MainActivity.kt
+++ app/src/main/java/id/codepanda/instagramclone/MainActivity.kt
@@ -3,20 +3,39 @@ package id.codepanda.instagramclone
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
 import id.codepanda.instagramclone.util.showNotif
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var itemHome: MenuItem
+    lateinit var itemSearch: MenuItem
+    lateinit var itemUpload: MenuItem
+    lateinit var itemFavourite : MenuItem
+    lateinit var itemProfile : MenuItem
+
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        menuIconReset()
         when (item.itemId) {
             R.id.navigation_home -> {
+                item.icon = getDrawable(R.drawable.ic_home)
                 return@OnNavigationItemSelectedListener true
             }
-            R.id.navigation_dashboard -> {
+            R.id.navigation_search-> {
+                item.icon = getDrawable(R.drawable.ic_search_bold)
                 return@OnNavigationItemSelectedListener true
             }
-            R.id.navigation_notifications -> {
+            R.id.navigation_upload-> {
+                item.icon = getDrawable(R.drawable.ic_add_circle)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_notification-> {
+                item.icon = getDrawable(R.drawable.ic_favorite)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_profile-> {
+                item.icon = getDrawable(R.drawable.ic_person)
                 return@OnNavigationItemSelectedListener true
             }
         }
@@ -28,6 +47,26 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        menuItemBinding()
         notif_button.setOnClickListener { showNotif(this, "Test", "Lorem ipsum dolor sir amet Lorem ipsum dolor sir amet Lorem ipsum dolor sir amet", MainActivity::class.java) }
+    }
+
+    private fun menuItemBinding(){
+        itemHome= navigation.menu.findItem(R.id.navigation_home)
+        itemSearch= navigation.menu.findItem(R.id.navigation_search)
+        itemUpload= navigation.menu.findItem(R.id.navigation_upload)
+        itemFavourite = navigation.menu.findItem(R.id.navigation_notification)
+        itemProfile= navigation.menu.findItem(R.id.navigation_profile)
+
+        //default icon
+        itemHome.icon = getDrawable(R.drawable.ic_home)
+    }
+
+    private fun menuIconReset(){
+        itemHome.icon = getDrawable(R.drawable.ic_home_border)
+        itemSearch.icon = getDrawable(R.drawable.ic_search)
+        itemUpload.icon = getDrawable(R.drawable.ic_add_circle_outline)
+        itemFavourite.icon = getDrawable(R.drawable.ic_favorite_border)
+        itemProfile.icon = getDrawable(R.drawable.ic_person_outline)
     }
 }
