c3a94bbfea5bc227e3890b9a7861a4c62c51cd36
Vito Rizki Imanda <vitorizkiimanda@gmail.com>
2019-05-20 11:50:33 +0700

    framgent declared

--- app/src/main/java/id/codepanda/instagramclone/MainActivity.kt
+++ app/src/main/java/id/codepanda/instagramclone/MainActivity.kt
@@ -2,8 +2,13 @@ package id.codepanda.instagramclone
 
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
 import android.view.MenuItem
+import id.codepanda.instagramclone.mainFragments.HomeFragment
+import id.codepanda.instagramclone.mainFragments.NotificationFragment
+import id.codepanda.instagramclone.mainFragments.ProfileFragment
+import id.codepanda.instagramclone.mainFragments.SearchFragment
 import id.codepanda.instagramclone.util.showNotif
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -14,16 +19,22 @@ class MainActivity : AppCompatActivity() {
     lateinit var itemUpload: MenuItem
     lateinit var itemFavourite : MenuItem
     lateinit var itemProfile : MenuItem
+    private val mHomeFragment: Fragment = HomeFragment()
+    private val mSearchFragment: Fragment = SearchFragment()
+    private val mNotificationFragment: Fragment = NotificationFragment()
+    private val mProfileFragment: Fragment = ProfileFragment()
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         menuIconReset()
         when (item.itemId) {
             R.id.navigation_home -> {
                 item.icon = getDrawable(R.drawable.ic_home)
+                commitFragment(mHomeFragment)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_search-> {
                 item.icon = getDrawable(R.drawable.ic_search_bold)
+                commitFragment(mSearchFragment)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_upload-> {
@@ -32,10 +43,12 @@ class MainActivity : AppCompatActivity() {
             }
             R.id.navigation_notification-> {
                 item.icon = getDrawable(R.drawable.ic_favorite)
+                commitFragment(mNotificationFragment)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_profile-> {
                 item.icon = getDrawable(R.drawable.ic_person)
+                commitFragment(mProfileFragment)
                 return@OnNavigationItemSelectedListener true
             }
         }
@@ -48,7 +61,7 @@ class MainActivity : AppCompatActivity() {
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
         menuItemBinding()
-        notif_button.setOnClickListener { showNotif(this, "Test", "Lorem ipsum dolor sir amet Lorem ipsum dolor sir amet Lorem ipsum dolor sir amet", MainActivity::class.java) }
+        navigation.selectedItemId = R.id.navigation_home
     }
 
     private fun menuItemBinding(){
@@ -68,5 +81,12 @@ class MainActivity : AppCompatActivity() {
         itemUpload.icon = getDrawable(R.drawable.ic_add_circle_outline)
         itemFavourite.icon = getDrawable(R.drawable.ic_favorite_border)
         itemProfile.icon = getDrawable(R.drawable.ic_person_outline)
+    }
+
+    private fun commitFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.main_container, fragment)
+            .commit()
     }
 }
